ac13850b37a5f638958349dbd823e3651b0ed899
Hamza Yassine <ihamzayassine@gmail.com>
2024-03-06 21:23:52 +0100

    Task 3 done

---
+++ models/__init__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Represent a Base class that defines all common attributes/methods for other classes.
+"""
+import json
+import uuid
+from datetime import datetime
+
+
+class Base:
+	"""
+	Manage and define all common attributes/methods for other classes.
+	"""
+	def __init__(self):
+		"""
+		Class constructor (initialization)
+
+		Args:
+			id (int): the id attribute of a class.
+		"""
+		self.id = uuid.uuid4()
+		self.created_at = datetime.now()
+		self.updated_at = datetime.now()
+
+	def __str__(self):
+		"""
+		Return a string representation of the BaseModel instance.
+		"""
+		return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+	
+	def save(self):
+		"""
+		Updates the public instance attribute updated_at with the current datetime.
+		"""
+		self.updated_at = datetime.now()
+
+	def to_dict(self):
+		"""
+		Returns a dictionary containing all keys/values of __dict__ of the instance.
+		"""
+		obj_dict = self.__dict__.copy()
+		obj_dict['__class__'] = self.__class__.__name__
+		obj_dict['created_at'] = self.created_at.isoformat()
+		obj_dict['updated_at'] = self.updated_at.isoformat()
+		return obj_dict
